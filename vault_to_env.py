1f98b3e5d22d45eec06d14704903a745ace42cc1
Grant Zukel <admin@idevops.io>
2021-02-27 11:08:52 -0700

    using curl for conda install

--- vault_to_env.py
+++ vault_to_env.py
@@ -5,7 +5,7 @@ import os
 import urllib3
 http = urllib3.PoolManager()
 #os.environ["VAULT_DNS"] = "http://vault.idevops.io/v1/kv/data/"
-r = http.request('GET','{vault_dns}{vault_path}'.format(vault_dns=os.environ["VAULT_DNS"],vault_path=os.environ["VAULT_PATH"]),headers={"X-Vault-Token": os.environ['VAULT_TOKEN']})
+r = http.request('GET','{vault_dns}{vault_path}'.format(vault_dns=os.environ["VAULT_DNS"],vault_path=os.environ["VAULT_PATH"]),headers={"X-Vault-Token": os.environ['SECRET_VAULT_TOKEN']})
 vars_return = json.loads(r.data.decode('utf-8'))["data"]["data"]
 temp_secrets = open("tmp_secrets", "w")
 for var in vars_return:
