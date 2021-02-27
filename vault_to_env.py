0ff3f8c3190965b7da70ef1403bdec29edb87686
Grant Zukel <admin@idevops.io>
2021-02-27 10:41:38 -0700

    added variable source script

---
+++ vault_to_env.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+import os
+import json
+import os
+import urllib3
+http = urllib3.PoolManager()
+#os.environ["VAULT_DNS"] = "http://vault.idevops.io/v1/kv/data/"
+r = http.request('GET','{vault_dns}{vault_path}'.format(vault_dns=os.environ["VAULT_DNS"],vault_path=os.environ["VAULT_PATH"]),headers={"X-Vault-Token": os.environ['VAULT_TOKEN']})
+vars_return = json.loads(r.data.decode('utf-8'))["data"]["data"]
+temp_secrets = open("tmp_secrets", "w")
+for var in vars_return:
+        temp_secrets.write("export {key}={values} \n".format(key=var, values=vars_return[var]))
+temp_secrets.close()
