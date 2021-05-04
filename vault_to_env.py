16231efb295230eb9fba630f90a2ae91fd24a657
hquach <hquach@idevops.io>
2021-05-04 00:31:03 -0400

    Updated vault to env

--- vault_to_env.py
+++ vault_to_env.py
@@ -11,3 +11,17 @@ temp_secrets = open("tmp_secrets", "w")
 for var in vars_return:
         temp_secrets.write("export {key}='{values}' \n".format(key=var, values=vars_return[var]))
 temp_secrets.close()
+
+f_path = os.environ["APPLICATION_PATH"] + "/manifests/deployment.template.yml"
+f = open(f_path, 'r')
+new_lines = []
+for line in f:
+    for json_var in vars_return:
+        json_with_extra_stuff = "-=" + json_var + "=-"
+        line = line.replace(json_with_extra_stuff, vars_return[json_var])
+    new_lines.append(line)
+new_lines = "".join(new_lines)
+f_path = os.environ["APPLICATION_PATH"] + "/manifests/deployment.yml"
+test = open(f_path, "w")
+test.write(new_lines)
+test.close()
