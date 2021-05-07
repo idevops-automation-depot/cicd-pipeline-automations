5ed8b35f2a80ab2e6abad4273878620fd7182917
Alexis Beltran <abeltranmeza@idevops.io>
2021-05-07 20:25:40 +0000

    Updated sonarqube_create_project.py
    adding os.environ

--- sonarqube_create_project.py
+++ sonarqube_create_project.py
@@ -2,11 +2,12 @@
 import requests 
 import pprint
 import json
+import os
 from requests.auth import HTTPBasicAuth
 
 
 
-URL = 'http://localhost:9000/api/projects/create?project=org.sonarqube:' +["APP_NAME"] + '&name=' + ["APP_NAME"]
+URL = 'http://localhost:9000/api/projects/create?project=org.sonarqube:' +os.environ["APP_NAME"] + '&name=' + os.environ["APP_NAME"]
 
 
 get_request = requests.post(URL, auth=HTTPBasicAuth('admin','admin'))
