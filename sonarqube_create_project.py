cdda6524b44f62974a0e50b51ac6210ceb3c6693
Alexis Beltran <abeltranmeza@idevops.io>
2021-05-18 03:35:25 +0000

    Updated sonarqube_create_project.py

--- sonarqube_create_project.py
+++ sonarqube_create_project.py
@@ -7,7 +7,7 @@ from requests.auth import HTTPBasicAuth
 
 
 
-URL = 'http://localhost:9000/api/projects/create?project=org.sonarqube:' +os.environ["APP_NAME"] + '&name=' + os.environ["APP_NAME"]
+URL = 'http://172.17.0.1:9000/api/projects/create?project=org.sonarqube:' +os.environ["APP_NAME"] + '&name=' + os.environ["APP_NAME"]
 
 
 get_request = requests.post(URL, auth=HTTPBasicAuth('admin','admin'))
