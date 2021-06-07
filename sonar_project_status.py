d496a2e0b64f17a4de0dbd8a7a0ca02dbb47b213
Alexis <abeltranmeza@idevops.io>
2021-06-06 22:18:00 -0400

    overhaul change for sonar project status py

--- sonar_project_status.py
+++ sonar_project_status.py
@@ -1,4 +1,7 @@
+
+
 #!/usr/bin/env python
+from typing import ItemsView
 import requests 
 import pprint
 import json
@@ -7,54 +10,48 @@ from requests.auth import HTTPBasicAuth
 from prettytable import PrettyTable
 
 
-table = PrettyTable()
-# must be one of: [INFO, MINOR, MAJOR, CRITICAL, BLOCKER]
-
 
 info_type = ['MINOR', 'MAJOR', 'CRITICAL', 'BLOCKER']
 
-for info_type_items in info_type:
-    URL = 'http://172.17.0.1:9000/api/issues/search?pageSize100&severities='+ info_type_items +'&componentKeys=org.sonarqube:' + os.environ["APP_NAME"]
-
-    get_request = requests.get(URL, auth=HTTPBasicAuth('admin','admin'))
-
-    data = get_request.json()
-    
-
-    #pprint.pprint(data)
-
-    issues = data["issues"]
-
-    for x in issues:
-        item = x.keys()
-        header = (list(item))
-        
-
-    listfoo = []
-    for y in issues:
-        item2 = y.values()
-        rows = list(item2)
-        listfoo.append(list(item2))
-        
-
- 
-    #print (listfoo)
-    #print(header)
-    #print("total number of items " + str(rows) + " for " + info_type_items )
-    #print("total number of items " + str(len(listfoo)) + " for " + info_type_items )
-    
-    table.field_names = header 
-    #print(rows)
-    #table.add_row(rows)
-    for z in listfoo:
-        table.add_row(z)
-        #listfoo = []
+def create_table(table, field_name, rows):
+    #print(table)
+    table = PrettyTable()
+    table.field_names = field_name
+    for row in rows:
+        table.add_row(row)
     print(table)
-print("\n")
-print("\n")
-print("\n")
-print("\n")
-print("\n")
-print("\n")
-print(table)
-#pprint.pprint(issues)
+
+def info_type_issues(issue):
+  URL = 'http://172.17.0.1:9000/api/issues/search?pageSize100&severities='+ info_type +'&componentKeys=org.sonarqube:' + os.environ["APP_NAME"]
+  get_request = requests.get(URL, auth=HTTPBasicAuth('admin','admin'))
+  data = get_request.json()
+  #pprint.pprint(data)
+  issues = data["issues"]
+  return issues
+
+def pull_keys(issue):
+    for x in info_type_issues(issue):
+        item = x.keys()
+        head = list(item)
+        num = (len(head))
+        return head, num, issue
+        break
+
+def pull_values(issue):
+    list1 = []
+    for x in info_type_issues(issue):
+        item_values = x.values()
+        list1.append(list(item_values))
+    return list1
+
+for issue in info_type:
+    if pull_keys(issue) == None:
+        print("no " +issue +" issues found")
+    else:
+        print("\n" + issue + " Page Report")
+        result1 = pull_keys(issue)
+        array_list = result1[0]
+        type_of_issue = result1[2]
+        result2 = pull_values(issue)
+        rows = result2
+        create_table(type_of_issue,array_list, rows)
