5d7b29ae51c22d6416d8f8504cfdfbba2ae663bd
Alexis <abeltranmeza@idevops.io>
2021-06-06 22:29:52 -0400

    edit sonar project status py

--- sonar_project_status.py
+++ sonar_project_status.py
@@ -22,7 +22,7 @@ def create_table(table, field_name, rows):
     print(table)
 
 def info_type_issues(issue):
-  URL = 'http://172.17.0.1:9000/api/issues/search?pageSize100&severities='+ str(info_type) +'&componentKeys=org.sonarqube:' + os.environ["APP_NAME"]
+  URL = 'http://172.17.0.1:9000/api/issues/search?pageSize100&severities='+ str(issue) +'&componentKeys=org.sonarqube:' + os.environ["APP_NAME"]
   get_request = requests.get(URL, auth=HTTPBasicAuth('admin','admin'))
   data = get_request.json()
   #pprint.pprint(data)
